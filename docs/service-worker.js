e7c1dbc6a6bc067f642bdec01f059fa8818745a8
theSoberSobber <akshatshrivastava0010@gmail.com>
2024-07-29 19:15:52 +0530

    [Feat.] PWA, SEO

---
+++ docs/service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'leetcode-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './leetcode_problems.json',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(keyList =>
+            Promise.all(keyList.map(key => {
+                if (!cacheWhitelist.includes(key)) {
+                    return caches.delete(key);
+                }
+            }))
+        )
+    );
+});
